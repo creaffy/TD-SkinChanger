27294201921038a64faac4fe35930ae420dc93a5
creaffy <81998541+creaffy@users.noreply.github.com>
2023-01-29 13:20:07 +0100

    v2

--- main.py
+++ main.py
@@ -7,73 +7,89 @@ KEY = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "SOFTWARE\\ShibaFlow\\Tactical Du
 with open("cosmetics.json") as file:
     cosmetics = json.load(file)
 
-def editKey(slot, value):
+def editKey(raw_slot:str, value:int):
+    match raw_slot:
+        case "OUTFIT": slot = "Skin_h2089423610"
+        case "KNIFE": slot = "Knife_h221345514"
+        case "AK": slot = "AK_h5861935"
     winreg.SetValueEx(KEY, slot, 0, winreg.REG_DWORD, value)
 
-def readKey(slot):
+def readKey(slot:str):
     return winreg.QueryValueEx(KEY, slot)[0]
 
-def getName(type, id):
+def getName(type:str, id:int):
     for skin in cosmetics[type]:
         if skin[0] == int(id):
             return skin[1]
 
 def showCommands():
-    print("ALL COMMANDS:\n/SKIN - change your skin by id\n/KNIFE - change your knife by id\n/SKINS - skins ids list\n/KNIVES - knives ids list\n/CURRENT - your currently equipped skins\n/HELP - this menu")
+    print("** ALL COMMANDS")
+    print("- /SET -> slot -> id")
+    print("- /SLOTS")
+    print("- /IDS -> slot")
+    print("- /CURRENT")
+    print("- /HELP")
 
 def newCommand():
-    global command 
     command = input("\n>>> ").upper()
+
     match command:
-        case "/SKINS":
+        case "/SET":
             try:
-                for skin in cosmetics['skins']:
-                    print(f"{str(skin[0]).rjust(2, '0')} : {skin[1]}")
-            except Exception as e:
-                print(f"FATAL ERROR: \n{e}")
-        case "/KNIVES":
-            try:
-                for skin in cosmetics['knives']:
-                    print(f"{str(skin[0]).rjust(2, '0')} : {skin[1]}")
-            except Exception as e:
-                print(f"FATAL ERROR: \n{e}")
-        case "/SKIN":
-            try:
-                print('Enter Skin ID, type "ABORT" to cancel.')
-                choice = input("ID: ")
-                if choice.upper() == "ABORT":
+                slot_choice = input("SLOT: ").upper()
+                if slot_choice == "ABORT":
                     newCommand()
                 else:
-                    editKey("Skin_h2089423610", int(choice))
-                    print(f"Your skin has been changed to {getName('skins', int(choice))}")
+                    id_choice = input("ID: ")
+                    if id_choice == "ABORT":
+                        newCommand()
+                    else:
+                        editKey(slot_choice, int(id_choice))
+                        print(f"Your skin has been changed to {getName(slot_choice, int(id_choice))}")
+
             except Exception as e:
                 print(f"FATAL ERROR: \n{e}")
-        case "/KNIFE":
+        
+        case "/SLOTS":
             try:
-                choice = input("ID: ")
-                if choice.upper() == "ABORT":
+                print("*** SUPPORTED SLOTS")
+                print("- OUTFIT")
+                print("- KNIFE")
+                print("- AK")
+            except Exception as e:
+                print(f"FATAL ERROR: \n{e}")
+            
+        case "/IDS":
+            try:
+                slot_choice = input("SLOT: ").upper()
+                if slot_choice == "ABORT":
                     newCommand()
                 else:
-                    editKey("Knife_h221345514", int(choice))
-                    print(f"Your knife has been changed to {getName('knives', int(choice))}")
+                    for skin in cosmetics[slot_choice]:
+                        print(f"{str(skin[0]).rjust(2, '0')} : {skin[1]}")
             except Exception as e:
                 print(f"FATAL ERROR: \n{e}")
+
         case "/CURRENT":
             try:
-                print(f'SKIN: {str(readKey("Skin_h2089423610")).rjust(2, "0")} | {getName("skins", readKey("Skin_h2089423610"))}')
-                print(f'KNIFE: {str(readKey("Knife_h221345514")).rjust(2, "0")} | {getName("knives", readKey("Knife_h221345514"))}')
+                print(f'SKIN: {str(readKey("Skin_h2089423610")).rjust(2, "0")} | {getName("OUTFIT", readKey("Skin_h2089423610"))}')
+                print(f'KNIFE: {str(readKey("Knife_h221345514")).rjust(2, "0")} | {getName("KNIFE", readKey("Knife_h221345514"))}')
+                print(f'AK: {str(readKey("AK_h5861935")).rjust(2, "0")} | {getName("AK", readKey("AK_h5861935"))}')
             except Exception as e:
                 print(f"FATAL ERROR: \n{e}")
+
         case "/HELP":
             try:
                 showCommands()
             except Exception as e:
                 print(f"FATAL ERROR: \n{e}")
+
         case _:
             try:
                 print("Unknown Command. Use /HELP for a list.")
             except Exception as e:
                 print(f"FATAL ERROR: \n{e}")
+
     newCommand()
 
 print("TacticalDuty.io Skin Changer by Creaffy")
